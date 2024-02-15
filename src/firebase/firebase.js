9f95432ecbcf51b520c8211e2622e5600f29761c
AbdulBasit <abdulbasitkhan2733@gmail.com>
2024-02-15 13:28:30 +0530

    setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,13 @@ import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMaKQ_yncCuOK4Eo-HII0hFodo1VbfrA0",
-  authDomain: "insta-clone-yt-a4994.firebaseapp.com",
-  projectId: "insta-clone-yt-a4994",
-  storageBucket: "insta-clone-yt-a4994.appspot.com",
-  messagingSenderId: "319407394451",
-  appId: "1:319407394451:web:cecf07f55a4d409568c766",
-  measurementId: "G-EDWXJQ7MJ3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
